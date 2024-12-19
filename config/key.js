113d38f21169f980a787298104998c1fee6da72b
utzmankazeem <usman4kay@gmail.com>
2024-12-19 11:38:57 +0100

    modify config

---
+++ config/key.js
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import mongoose from "mongoose";
+const isProduction = process.env.NODE_ENV === 'production'
+const dbUri = isProduction ? process.env.DATABASE_URI : process.env.DATABASE_URI_LOCAL
+
+const connectDB = async () => {
+    mongoose.set('strictQuery', true)
+    try {
+        await mongoose.connect(dbUri, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            family:4
+        });
+    } catch (err) {
+        throw err
+    }
+}
+
+export default connectDB
